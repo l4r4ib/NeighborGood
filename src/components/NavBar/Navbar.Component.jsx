7c4a92fc28bea6e4158b7a69fd01e5ee1091cada
laraib shuja <105169013+l4r4ib@users.noreply.github.com>
2024-07-02 18:48:36 +0530

    Update Navbar.Component.jsx

--- src/components/NavBar/Navbar.Component.jsx
+++ src/components/NavBar/Navbar.Component.jsx
@@ -1,6 +1,5 @@
-// src/components/Navbar.js
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
